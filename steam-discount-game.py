c3e6b91df0fb8d529d0c74b1b9f4aebc512e19a6
Ernket <35891165+Ernket@users.noreply.github.com>
2018-12-03 23:59:56 +0800

    Update steam-discount-game.py

--- steam-discount-game.py
+++ steam-discount-game.py
@@ -7,27 +7,30 @@ pageid = 1
 
 file = open("steam.txt","a")
 while pageid<10:
-    url = "https://store.steampowered.com/search/?specials=1&page=" + str(pageid)
-    res = requests.get(url)
-    soup = bs4.BeautifulSoup(res.text)
-    contents = soup.select('div[class="responsive_search_name_combined"]')
+	url = "https://store.steampowered.com/search/?specials=1&page=" + str(pageid)
+	res = requests.get(url)
+	soup = bs4.BeautifulSoup(res.text)
+	contents = soup.select('div[class="responsive_search_name_combined"]')
 
-    for content in contents:
-        try:
-            name = content.find("span",class_="title").string.strip()
-            print(name)
-            file.write(name+"\n")
-            file.close
-        except:
-            print(name.encode('utf-8'))
-            urlname = name.encode('utf-8')
-            newname1 = str(urlname).replace('xe2','')
-            newname2 = str(newname1).replace('x84','')
-            newname3 = str(newname2).replace('xa2','')
-            newname4 = str(newname3).replace('xc2','')
-            newname5 = str(newname4).replace('xe2','')
-            newname6 = str(newname5).replace('xae','')
-            newname7 = str(newname6).replace('\\','')
-            file.write(str(newname7)+"\n")
-            file.close
-    pageid = pageid + 1
+	for content in contents:
+		try:
+			name = content.find("span",class_="title").string.strip()
+			print(name)
+			file.write(name+"\n")
+			file.close
+		except:
+			print(type(name))
+			print(name.encode('utf-8'))
+			urlname = name.encode('utf-8')
+			newname1 = str(urlname).replace('xe2','')
+			newname2 = str(newname1).replace('x84','')
+			newname3 = str(newname2).replace('xa2','')
+			newname4 = str(newname3).replace('xc2','')
+			newname5 = str(newname4).replace('xe2','')
+			newname6 = str(newname5).replace('xae','')
+			newname7 = str(newname6).replace('\\','')
+			newname8 = str(newname7).replace('b\'','')
+			newname9 = str(newname8).replace('\'','')
+			file.write(str(newname9)+"\n")
+			file.close
+	pageid = pageid + 1
