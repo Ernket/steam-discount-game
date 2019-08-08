d65837fa3be651167b80758f084b03499799f912
Ernket <35891165+Ernket@users.noreply.github.com>
2019-08-08 16:22:53 +0800

    Update steam-discount-game.py

--- steam-discount-game.py
+++ steam-discount-game.py
@@ -2,35 +2,51 @@
 import requests
 import bs4
 import urllib
-
+import sys
+import re
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030')
 pageid = 1
+hd="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"
+header={"User-Agent":hd}
 
-file = open("steam.txt","a")
-while pageid<10:
-	url = "https://store.steampowered.com/search/?specials=1&page=" + str(pageid)
-	res = requests.get(url)
-	soup = bs4.BeautifulSoup(res.text)
-	contents = soup.select('div[class="responsive_search_name_combined"]')
+while pageid<4: #只爬取3页
+	url = "https://store.steampowered.com/search/?specials=1&page="+str(pageid)
 
-	for content in contents:
-		try:
-			name = content.find("span",class_="title").string.strip()
-			print(name)
-			file.write(name+"\n")
-			file.close
-		except:
-			print(type(name))
-			print(name.encode('utf-8'))
-			urlname = name.encode('utf-8')
-			newname1 = str(urlname).replace('xe2','')
-			newname2 = str(newname1).replace('x84','')
-			newname3 = str(newname2).replace('xa2','')
-			newname4 = str(newname3).replace('xc2','')
-			newname5 = str(newname4).replace('xe2','')
-			newname6 = str(newname5).replace('xae','')
-			newname7 = str(newname6).replace('\\','')
-			newname8 = str(newname7).replace('b\'','')
-			newname9 = str(newname8).replace('\'','')
-			file.write(str(newname9)+"\n")
-			file.close
-	pageid = pageid + 1
+	res = requests.get(url,headers=header)
+
+	soup = bs4.BeautifulSoup(res.text,'html5lib')
+
+	contents=soup.find_all("span",class_="title") #游戏名
+	money=soup.find_all("div",class_="col search_price discounted responsive_secondrow") #原价+打折后
+	emoney=soup.find_all("strike") #原价
+
+	a=0
+	y_money=[]
+	d_money=[]
+	game_name=[]
+
+	for i in money:
+		#获取文本
+		i=i.get_text() 
+		discount=emoney[a].get_text()
+		contest=contents[a].get_text()
+
+		i=i.replace(discount,'') #原价+打折后的价格，通过replace来将原价删除，只剩打折后
+		ym=re.findall('\d+',i) #正则匹配数字,原价
+		dm=re.findall('\d+',discount) #打折后
+		y_money.append(ym)
+		d_money.append(dm)
+		game_name.append(contest)
+		a+=1
+	a=0
+
+	file = open("steam.txt","a",encoding="gb18030")
+
+	for i in range(25):
+		text=("游戏名: "+game_name[a]+"  原价为: "+d_money[a][0]+"  打折后: "+y_money[a][0])
+		#print(text)
+		file.write("\n"+text+"\n")
+		a+=1
+	file.close()
+	pageid+=1
